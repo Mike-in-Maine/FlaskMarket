eef46bf25a456b96f523c3bd6ea80651233a4299
retiredtotuscany <retiredtotuscany@gmail.com>
2022-05-07 20:30:29 +0200

    Initial commit

---
+++ market.py
@@ -0,0 +1,123 @@
+from flask import Flask
+from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy
+from sqlalchemy import create_engine
+import pandas as pd
+import requests
+import xml.etree.ElementTree as ET
+import xmltodict, json
+import untangle
+import pymysql
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///market.db'
+db = SQLAlchemy(app)
+
+app2 = Flask(__name__)
+app2.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///market.db'
+db2 = SQLAlchemy(app)
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable = False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable = False, unique = True)
+
+    def __repr__(self):
+        return f'Item {self.Item}'
+
+@app.route('/')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/market')
+def market_page():
+
+    items = Item.query.all()
+    return render_template('market.html', items = items, citys = citys)
+
+def get_abe_API_neworders():
+    df = pd.DataFrame()
+    data = """
+    <?xml version="1.0" encoding="ISO-8859-1"?>
+    <orderUpdateRequest version="1.1">
+        <action name="getAllNewOrders">
+            <username>irishbooksellers</username>
+            <password>ef624a8bd5a843cda651</password>
+        </action>
+    </orderUpdateRequest>
+    """
+    headers = {'username': 'irishbooksellers','password': 'ef624a8bd5a843cda651'}
+    response = requests.get('https://orderupdate.abebooks.com:10003', data=data, headers=headers)
+    print(response.text)
+    root = ET.fromstring(response.text)
+    dump = xmltodict.parse(response.text)
+
+    obj = untangle.parse(response.text)
+    emails = []
+    citys = []
+    codes = []
+    countrys = []
+    names = []
+    phones = []
+    regions = []
+    streets = []
+    street2s = []
+
+
+    for po in root.findall('purchaseOrderList'):
+        for po2 in po.findall('purchaseOrder'):
+            for po3 in po2.findall('buyer'):
+                for po4 in po3.findall('mailingAddress'):
+                    city = po4.find('city').text
+                    code = po4.find('code').text
+                    country = po4.find('country').text
+                    name = po4.find('name').text
+                    phone = po4.find('phone').text
+                    region = po4.find('region').text
+                    street = po4.find('street').text
+                    street2 = po4.find('street2').text
+                    citys.append(city)
+                    codes.append(code)
+                    countrys.append(country)
+                    names.append(name)
+                    phones.append(phone)
+                    regions.append(region)
+                    streets.append(street)
+                    street2s.append(street2)
+    print(citys)
+    print(codes)
+    print(countrys)
+    print(phones)
+    print(names)
+    print(phones)
+    print(regions)
+    print(streets)
+    print(street2s)
+
+    print(len(citys))
+    print(len(codes))
+    print(len(countrys))
+    print(len(names))
+    print(len(phones))
+    print(len(regions))
+    print(len(streets))
+    print(len(street2s))
+
+    yield citys
+
+
+#sql connection to dreamhost
+engine = sqlalchemy.create_engine('mysql+pymysql://miky1973:itff2020@mysql.irish-booksellers.com:3306/irishbooksellers')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
